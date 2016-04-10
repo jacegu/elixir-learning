7d6be9dde46d7b928196d1d4b92c85257f3449a6
Javier Acero <j4cegu@gmail.com>
2016-04-10 09:31:52 +0200

    Sequence OTP server example

---
+++ programming-elixir/sequence/lib/sequence/server.ex
@@ -0,0 +1,12 @@
+defmodule Sequence.Server do
+  use GenServer
+
+  def handle_call(:next_number, _from, current_number) do
+    {:reply, current_number, current_number+1}
+  end
+
+  def handle_call({:reset_number, new_number}, _from, _current_number) do
+    {:reply, new_number, new_number}
+  end
+
+end
