d65f7e4164d43f7a57c856f6a61b1431a8411f2f
Javier Acero <j4cegu@gmail.com>
2016-04-26 09:30:46 +0200

    Add supervisor

--- programming-elixir/sequence/lib/sequence.ex
+++ programming-elixir/sequence/lib/sequence.ex
@@ -1,2 +1,15 @@
 defmodule Sequence do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+      worker(Sequence.Server, [100])
+    ]
+
+    opts = [ strategy: :one_for_one, name: Sequence.Supervisor ]
+
+    {:ok, _pid} = Supervisor.start_link(children, opts)
+  end
 end
