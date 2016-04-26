698659963c7b3de6085c2a091682d9175d97df5f
Javier Acero <j4cegu@gmail.com>
2016-04-26 09:30:32 +0200

    Add wrapper functions for GenServer calls

--- programming-elixir/sequence/lib/sequence/server.ex
+++ programming-elixir/sequence/lib/sequence/server.ex
@@ -1,6 +1,22 @@
 defmodule Sequence.Server do
   use GenServer
 
+  def start_link(initial_number) do
+    GenServer.start_link(__MODULE__, initial_number, name: __MODULE__)
+  end
+
+  def next_number do
+    GenServer.call(__MODULE__, :next_number)
+  end
+
+  def reset_number(new_number) do
+    GenServer.call(__MODULE__, {:reset_number, new_number})
+  end
+
+  def increment_number(delta) do
+    GenServer.cast(__MODULE__, {:increment_number, delta})
+  end
+
   def handle_call(:next_number, _from, current_number) do
     {:reply, current_number, current_number+1}
   end
